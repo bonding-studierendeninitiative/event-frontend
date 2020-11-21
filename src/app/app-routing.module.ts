e3371fb9d54ba0bfa64d33b25802f65d62b96a80
untersehers <sarah.unterseher@web.de>
2020-11-21 13:05:12 +0100

    Initialisierung

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EventListComponent } from './event-list/event-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent},
+  { path: 'event-list', component: EventListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'event-frontend';
+  name = "Sarah";
+  hobbies = ["Garde", "Schwimmen"];
 }
