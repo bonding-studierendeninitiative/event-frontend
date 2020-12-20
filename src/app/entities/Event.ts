2db027afff6b3ab5cfce8c213c2d42ab8f996024
Alexander Backes <alexander.backes@conemis.com>
2020-12-20 10:49:04 +0100

    Event Entity erstellt

---
+++ src/app/entities/Event.ts
@@ -0,0 +1,13 @@
+export interface Event {
+  Titel: string;
+  HSG: string;
+  ID: string;
+  Anmeldung: string;
+  StartDatum: string;
+  StartUhrzeit: string;
+  EndeDatum: string;
+  EndeUhrzeit: string;
+  Typ: string;
+  Ort: string;
+  WebText: string;
+}
