f22159786762474ffdb774764ba27301d610573c
untersehers <sarah.unterseher@web.de>
2021-02-05 16:56:28 +0100

    Component Drop-Down hinzugefügt

--- src/app/drop-down/drop-down.component.ts
+++ src/app/drop-down/drop-down.component.ts
@@ -1,46 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import {Component} from '@angular/core';
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
-  selector: 'select-form-example',
-  templateUrl: 'select-form-example.html',
+  selector: 'app-drop-down',
+  templateUrl: './drop-down.component.html',
   styleUrls: ['./drop-down.component.scss']
 })
 export class DropDownComponent implements OnInit {
 
-  constructor() {   interface Location {
-    value: string;
-    viewValue: string;
-  }
-  
-  /**
-   * @title Select in a form
-   */
-  
-    selectedValue: string;
-    selectedCar: string;
-  
-    foods: Location[] = [
-      {value: 'aachen', viewValue: 'Aachen'},
-      {value: 'bochum', viewValue: 'Bochum'},
-      {value: 'berlin', viewValue: 'Berlin'},
-      {value: 'braunschweig', viewValue: 'Braunschweig'},
-      {value: 'bremen', viewValue: 'Bremen'},
-      {value: 'dresden', viewValue: 'Dresden'},
-      {value: 'erlangen', viewValue: 'Erlangen'},
-      {value: 'hamburg', viewValue: 'Hamburg'},
-      {value: 'kaiserslautern', viewValue: 'Kaiserslautern'},
-      {value: 'karlsruhe', viewValue: 'Karlsruhe'},
-      {value: 'stuttgart', viewValue: 'Stuttgart'},
-
-    ];
-  }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectedValue: string;
+  selectedCar: string;
 
-
-
+  foods: Location[] = [
+    {value: 'aachen', viewValue: 'Aachen'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
 
 }
