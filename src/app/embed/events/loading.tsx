42b721af0f7045a282840b9f66c9e49aba423b96
mkrass-syntax <maximilian.krass@syntax-institut.de>
2024-02-26 19:22:16 +0100

    refactors ui

---
+++ src/app/embed/events/loading.tsx
@@ -0,0 +1,5 @@
+import { Loader2 } from "lucide-react";
+
+export default function EventsLoading() {
+  return <Loader2 className="animate-spin" />;
+}
--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -8,26 +8,24 @@ import { getTypeByCategory, strip } from "@/lib/events";
 
 export function _bondingEvent({ event }: { event: Event }) {
   return (
-    <div className="border-neutral-20 border shadow max-w-[45ch] flex flex-col rounded-3xl">
-      <div className="px-8 py-6 flex flex-col flex-grow gap-4">
-        <EventTitle title={event.title} />
-        <div className="flex gap-x-4 gap-y-2 items-center flex-wrap">
-          <EventDate {...event.startDate} />
-          {event.localGroup && <EventLocation location={event.localGroup} />}
-          <EventTypeBadge type={getTypeByCategory(event.type)} />
-        </div>
-        <EventDescription
-          className="flex-grow"
-          description={strip(event.description)}
-        />
-        <a
-          href={`https://www2.bonding.de/veranstaltungen.nsf/anmeldungstud?OpenForm&id=${event.id}`}
-          className="text-white bg-blue text-center rounded-full px-4 py-2 no-underline"
-          target="_blank"
-        >
-          Mehr Informationen & Anmeldung
-        </a>
+    <div className="border-neutral-20 border shadow max-w-[45ch] flex flex-col px-8 pt-6 pb-7 gap-4 rounded-3xl">
+      <EventTitle title={event.title} />
+      <div className="flex gap-x-4 gap-y-2 items-center flex-wrap">
+        <EventDate {...event.startDate} />
+        {event.localGroup && <EventLocation location={event.localGroup} />}
+        <EventTypeBadge type={getTypeByCategory(event.type)} />
       </div>
+      <EventDescription
+        className="flex-grow"
+        description={strip(event.description)}
+      />
+      <a
+        href={`https://www2.bonding.de/veranstaltungen.nsf/anmeldungstud?OpenForm&id=${event.id}`}
+        className="text-white bg-blue text-center rounded-full px-4 py-2 no-underline"
+        target="_blank"
+      >
+        Mehr Informationen & Anmeldung
+      </a>
     </div>
   );
 }
