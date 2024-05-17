6e8d250299e486df34daef071abb6fd37ef0e65c
maxkrass <maxkrass.com@gmail.com>
2024-05-17 11:48:04 +0200

    Updates Event.tsx to fix signup url

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -20,7 +20,7 @@ export function _bondingEvent({ event }: { event: Event }) {
         description={strip(event.description)}
       />
       <a
-        href={`https://www2.bonding.de/veranstaltungen.nsf/anmeldungstud?OpenForm&id=${event.id}`}
+        href={`https://www1.bonding.de/veranstaltungen.nsf/anmeldungstud?OpenForm&id=${event.id}`}
         className="text-white bg-blue text-center rounded-full px-4 py-2 no-underline"
         target="_blank"
       >
